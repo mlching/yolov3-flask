f1d9377dcea782bfef47d22a40aff6a8f8b6fd5c
Martin Lee <martinlee@Martins-MacBook-Air.local>
2023-08-10 18:57:44 +0800

    Changes to be committed: 	new file:   mod.py 	new file:   v8test.py 	new file:   yolov8.py

---
+++ v8test.py
@@ -0,0 +1,74 @@
+from ultralytics import YOLO
+import cv2
+import os
+import mod
+import csv
+import time
+
+# Load YOLO model
+model_name = "yolov8x.pt"
+model = YOLO(f"./weights/{model_name}")
+
+# Define input and output folders
+input = "testing2"
+input_folder = f'/Users/martinlee/{input}'  # Change this to the folder containing your input images
+output_folder = f'./output_images/{model_name}'  # Change this to the desired output folder for processed images
+
+# Get a list of image file names in the folder
+image_names = [f for f in os.listdir(input_folder) if f.lower().endswith(('.jpg', '.jpeg', '.png', 'JPG'))]
+
+# Define a function to extract the numeric part from the filename
+def extract_number(filename):
+    try:
+        return int(filename[len("photo"):filename.index(".")])
+    except ValueError:
+        return float('inf')  # Return a very large value for filenames that don't match the pattern
+
+# Sort the image file names based on the extracted numbers
+sorted_image_names = sorted(image_names, key=extract_number)
+print(sorted_image_names)
+
+
+# Create output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
+
+# Define output CSV file
+output_csv = f'./data/{model_name}_{input}_results.csv'  # Change this to the desired output CSV file name
+
+# Initialize CSV writer
+csv_file = open(output_csv, 'w', newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Image', 'Object', 'Distance', 'Location', 'Time'])
+
+# Process images in the input folder
+for image_filename in sorted_image_names:
+    if image_filename.endswith(('.jpg', '.jpeg', '.png', 'JPG')):
+        image_path = os.path.join(input_folder, image_filename)
+        frame = cv2.imread(image_path)
+
+        start = time.time()
+        prediction = model(frame)[0]
+        end = time.time() - start
+        data = prediction.boxes.data.tolist()
+        lists = []
+        for i in data:
+            text, lis = mod.give_direction(i[:4], prediction.names[i[5]], frame)
+            lists.append(lis)
+
+        for lis in lists:
+            if lis['movable']:
+                cv2.rectangle(frame, tuple(lis['bbox'][:2]), tuple(lis['bbox'][2:]), (0, 0, 255), 1)
+            else:
+                cv2.rectangle(frame, tuple(lis['bbox'][:2]), tuple(lis['bbox'][2:]), (0, 255, 0), 1)
+            cv2.putText(frame, f"Object:{lis['object']}, Distance:{lis['distance']}, Location:{lis['location']}",
+                        tuple([lis['bbox'][0], lis['bbox'][1]]), 0, 1, (0, 0, 0), 1)
+
+            # Save the processed image with bounding boxes in the output folder
+            output_image_path = os.path.join(output_folder, f"processed_{image_filename}")
+            cv2.imwrite(output_image_path, frame)
+
+            # Write data to CSV
+            csv_writer.writerow([image_filename, lis['object'], lis['distance'], lis['location'], end])
+
+# Close the CSV file
+csv_file.close()
---
+++ yolov8.py
@@ -0,0 +1,80 @@
+from ultralytics import YOLO
+from datetime import datetime
+import cv2
+import time
+import mod
+
+model = YOLO("./weights/yolov8s.pt")
+vid = cv2.VideoCapture(0)
+start_time = time.time()
+
+while True:
+    return_value, frame = vid.read()
+
+    if not return_value:
+        print("Failed to capture frame from webcam")
+        continue
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    image_filename = './data/captured_frame.jpg'
+    cv2.imwrite(image_filename, frame)
+
+    if time.time() - start_time >= 2:
+        # test from GCR
+        request_time = datetime.now()
+        print('Request sent time: ', request_time)
+
+        prediction = model(frame)[0]
+        data = prediction.boxes.data.tolist()
+        texts = []
+        lists = []
+        for i in data:
+            text, lis = mod.give_direction(i[:4], prediction.names[i[5]], frame)
+            if len(text) != 0:
+                texts.append(text)
+            lists.append(lis)
+        if len(texts) == 0:
+            print("No object detected.")
+        else:
+            print(texts)
+            # print(resp.json()["bbox"])
+        receive_time = datetime.now()
+        print('Response received: ', receive_time)
+        print('RTT: ', receive_time - request_time)
+        start_time = time.time()
+    else:
+        prediction = model(frame)[0]
+        data = prediction.boxes.data.tolist()
+        lists = []
+        for i in data:
+            text, lis = mod.give_direction(i[:4], prediction.names[i[5]], frame)
+            lists.append(lis)
+    height, width, _ = frame.shape
+    line1_pos = int(width * 20 / 100)
+    line2_pos = int(width * 80 / 100)
+
+    # Draw lines on the frame
+    green = (0, 255, 0)
+    black = (0, 0, 0)
+    red = (0, 0, 255)
+
+    #segmentation
+    cv2.line(frame, (line1_pos, 0), (line1_pos, height), black, 3)
+    cv2.line(frame, (line2_pos, 0), (line2_pos, height), black, 3)
+
+    #drawing bounding boxes
+    for lis in lists:
+        if lis['location'] == 'straight':
+            if lis['movable']:
+                if lis['distance'] <= 5:
+                    cv2.rectangle(frame, tuple(lis['bbox'][:2]), tuple(lis['bbox'][2:]), red, 1)
+                    cv2.putText(frame, f"Object:{lis['object']}, Distance:{lis['distance']}, Location:{lis['location']}", tuple([lis['bbox'][0], lis['bbox'][1]]), 0, 0.5, black, 1)
+            elif lis['distance'] <= 3:
+                cv2.rectangle(frame, tuple(lis['bbox'][:2]), tuple(lis['bbox'][2:]), green, 1)
+                cv2.putText(frame, f"Object:{lis['object']}, Distance:{lis['distance']}, Location:{lis['location']}", tuple([lis['bbox'][0], lis['bbox'][1]]), 0, 0.5, black, 1)
+        elif lis['movable']:
+            cv2.rectangle(frame, tuple(lis['bbox'][:2]), tuple(lis['bbox'][2:]), red, 1)
+            cv2.putText(frame, f"Object:{lis['object']}, Distance:{lis['distance']}, Location:{lis['location']}", tuple([lis['bbox'][0], lis['bbox'][1]]), 0, 0.5, black, 1)
+
+    cv2.imshow("Output Video", frame)

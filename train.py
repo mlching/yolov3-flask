bb2f2a7a59d4a64982314f8e0444c22dd5303994
mlching <1155158267@link.cuhk.edu.hk>
2023-08-30 17:40:28 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   train.py 	new file:   train.yaml

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8m.yaml')  # build a new model from YAML
+model = YOLO('yolov8m.yaml')
+model = YOLO('yolov8m.pt')# build a new model from YAML
+model = YOLO('yolov8m.yaml').load('yolov8m.pt')
 # Train the model
 
 if __name__ == '__main__':
-    result = model.train(data='custom1and3.yaml', epochs=100, imgsz=640, batch=16, pretrained=False)
+    result = model.train(data='custom1and3.yaml', epochs=100, imgsz=640, batch=16, pretrained=True, val=True)
     result.val()
